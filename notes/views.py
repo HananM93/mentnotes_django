50c39ac6152d4ab92934222cd56378971e5f72a0
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-19 12:08:42 -0600

    done with class,index,create in views file

--- notes/views.py
+++ notes/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from .models import Note
+from .serializers import NoteSerializer
 
 
 # Test View for Homepage
@@ -18,4 +20,14 @@ def home(request):
 class Notes(APIView):
     def get(self,request):
         note = Note.objects.all()
+        data = NoteSerializer(notes, many=True).data
+        return Response(serializer.data)
         
+    
+    def post(self,request):
+      data = request.data
+      if note.is_valid():
+        note.save()
+        return Response(note.data, status=status.HTTP_201_CREATED)
+      else:
+        return Response(note.errors, status=status.HTTP_400_BAD_REQUEST)

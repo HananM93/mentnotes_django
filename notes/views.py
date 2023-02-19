bbe4b3a52815660b07e0b5727cd6ae0a1ecfff95
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-19 10:25:50 -0600

    created model

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 # Test View for Homepage
@@ -12,3 +14,8 @@ def home(request):
 
 
 # Create your views here.
+
+class Notes(APIView):
+    def get(self,request):
+        note = Note.objects.all()
+        

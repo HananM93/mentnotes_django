ecd22eb8496362546301c5356b6ac0874631eec9
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-02-18 23:16:01 -0600

    created home route

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+
+# Test View for Homepage
+
+
+def home(request):
+  return HttpResponse('<h1> MY HOME PAGE </h1>')
+
 
 # Create your views here.
